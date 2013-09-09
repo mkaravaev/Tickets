ed9dc290206b4cab52c454ea8b56342d31081c59
mkaravaev <mkaravaev@gmail.com>
2013-09-09 09:36:45 +0400

    Initial commit

---
+++ config/application.rb
@@ -0,0 +1,19 @@
+require File.expand_path('../boot', __FILE__)
+
+require 'rails/all'
+
+# Require the gems listed in Gemfile, including any gems
+# you've limited to :test, :development, or :production.
+Bundler.require(:default, Rails.env)
+
+module Tickets
+  class Application < Rails::Application
+    config.generators do |g|
+     g.template_engine     :haml
+     g.test_framework      :rspec, :fixtures => true, :views => false
+     g.fixture_replacement :factory_girl, :dir => "spec/factories"
+   end
+  end
+end
+
+ 
---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,12 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key is used for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+# You can use `rake secret` to generate a secure secret key.
+
+# Make sure your secret_key_base is kept private
+# if you're sharing your code publicly.
+Tickets::Application.config.secret_key_base = '441364f0c1567770e64b17a598cf0aa0242036663fa5333298c79ac63730aa81c56a011ecc4e8dc4439305b60539dcb25340e82b5545798c57546cfe6209a1a3'
